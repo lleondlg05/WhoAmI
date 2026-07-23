58d81e2574641f991c2894b3e43da0e875d3e853
Luis C. <luisleon05.lcld@gmail.com>
2026-07-23 16:08:54 +0200

    Finished web

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Portfolio initialized");
+emailjs.init({
+    publicKey: "W-9zYPFoa3y-ygBFH",
+});
+
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    emailjs.sendForm(
+        "service_rdcwhrv",
+        "template_177mnwo",
+        contactForm
+    )
+    .then(() => {
+        alert("Message sent successfully!");
+        contactForm.reset();
+    })
+    .catch((error) => {
+        console.error(error);
+        alert("An error occurred while sending the message.");
+    });
+});
